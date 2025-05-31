931437207dc172b61305c1a00a2ba12e13a4f910
nitesh_chaudhary <devnitesh8740@outlook.com>
2025-06-01 00:25:55 +0530

    Backend Re-started

--- Backend/Tut2_gettingStarted_Deploy/index.js
+++ Backend/Tut2_gettingStarted_Deploy/index.js
@@ -1,14 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-app.get('/', function (req, res) {
-  res.send('Hello World! 2')
+app.get('/', (req, res) => {
+  res.send({name:'hello'})//auto detect header
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
